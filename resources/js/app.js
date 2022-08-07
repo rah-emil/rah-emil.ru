5a939dba978aac0ca836532aa2cdf1316425f7e8
Rah Emil <013131@mail.ru>
2022-08-07 13:12:12 +0300

    add easy-toggler plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import easySetup from 'easy-toggler';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // docs: https://github.com/rah-emil/easy-toggler#readme
+    easySetup();
+
+})
