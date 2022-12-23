f29270721c6ce3822a4a521dae4c983a747f3b36
Rah Emil <013131@mail.ru>
2022-12-23 09:24:08 +0300

    layout: setup styles (fonts, icons, tailwind)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-// ...
+import { Icon } from '@iconify/vue';
 </script>
 
 <template>
   <h1 class="font-bold underline text-5xl text-red-600">
-    Hello world!
+    Hello world! <Icon icon="mdi:github" />
   </h1>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+/* eslint-disable quote-props */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/*.{html,ts,js,vue,css}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Circie', 'sans-serif'],
+    },
+    screens: {
+      '3xs': '320px',
+      '2xs': '475px',
+      'xs': '576px',
+      'sm': '768px',
+      'md': '1024px',
+      'lg': '1200px',
+      '2lg': '1440px',
+      'xl': '1920px',
+    },
+    extend: {
+      colors: {
+        black: '#17191E',
+        dark: '#1F232E',
+        grey: '#3A4666',
+        primary: '#4069FF',
+        violet: '#BD3EC7',
+        yellow: '#FFD02C',
+      },
+    },
   },
   plugins: [],
 };
